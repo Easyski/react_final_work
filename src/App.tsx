d52d3ee68e98b8f17a02083ff82be87452b8b859
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-13 18:51:25 +0200

    feat(api): set mapbox token on initialisation of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import mapboxgl from "mapbox-gl";
 import { LngLatLike } from "mapbox-gl";
 import { FC, useState } from "react";
 import { Navigation, Sidebar } from "./components";
 
 export const App: FC = () => {
 	const [mapCenter, setMapCenter] = useState<LngLatLike>();
+	mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY as string;
+
 	return (
 		<div>
 			<Navigation fullScreen centerCoordinates={mapCenter} />
