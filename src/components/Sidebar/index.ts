4f815fec1a88a44a3c84096a8c59e0fa189fa8e0
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-13 18:57:32 +0200

    feat(import): update sidebar and navigation import

--- src/components/Sidebar/index.ts
+++ src/components/Sidebar/index.ts
@@ -1,5 +1,4 @@
 import SearchResults from "./SearchResults/SearchResults";
 import GeoSearch from "./GeoSearch/GeoSearch";
-import Sidebar from "./Sidebar";
 
-export { SearchResults, GeoSearch, Sidebar };
+export { SearchResults, GeoSearch };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
-import Navigation from "./Navigation/Navigation";
-import { Sidebar } from "./Sidebar";
+import Navigation from "./Map/Navigation/Navigation";
+import Sidebar from "./Sidebar/Sidebar";
 import StyledText from "./StyledText/StyledText";
 
 export { Navigation, StyledText, Sidebar };
