f7c35b1ac737d921b7b98fae38435bd572871ad4
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-18 15:19:21 +0200

    feat(editor): add editor config panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 // import mapboxgl from "mapbox-gl";
+import mapboxgl from "mapbox-gl";
 import { FC } from "react";
 import { Provider } from "react-redux";
-import { Navigation, Sidebar } from "./components";
+import { EditorConfig, Navigation, Sidebar } from "./components";
 import store from "./store";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY as string;
 
 export const App: FC = () => {
 	return (
 		<Provider store={store}>
-			<Navigation fullScreen />
+			<Navigation />
 			<Sidebar />
+			<EditorConfig />
 		</Provider>
 	);
 };
