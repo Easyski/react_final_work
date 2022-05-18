bb8eea0b0a78690849fead4f1cab4a34e5260f4a
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-18 16:48:30 +0200

    feat(sidebar): moved logic from Sidebar component and add descriptions

--- src/components/Sidebar/GeoSearch/GeoSearch.tsx
+++ src/components/Sidebar/GeoSearch/GeoSearch.tsx
@@ -1,11 +1,36 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 
 import { IGeoSearchTypes } from "./GeoSearch.types";
-import "./GeoSearch.css";
+import { useDispatch } from "react-redux";
 
-const GeoSearch: FC<IGeoSearchTypes> = ({ handleFetchResults }) => {
+import "./GeoSearch.css";
+import { setLocations } from "../../../store/slices/sidebarSlice";
+import { Coordinates, ILocation } from "../../types";
+import { useSelector } from "react-redux";
+
+const GeoSearch: FC<IGeoSearchTypes> = () => {
+	const dispatch = useDispatch();
+	const locations = useSelector((state: any) => state.sidebar.locations);
+
+	const inputRef = useRef<HTMLInputElement | null>(null);
+
+	/**
+	 * This function will trigger everytime the @param locations is empty.
+	 * It will clear the input field.
+	 */
+	useEffect(() => {
+		if (!locations.length && inputRef.current) inputRef.current.value = "";
+	}, [locations]);
+
+	/**
+	 * This function will be excecuted whenever the value of the input field
+	 * changes. It will use the Mapbox GL api to get the 5 most accurate
+	 * matches.
+	 *
+	 * @param event This is used to access the value of the input field.
+	 */
 	const handleSearchInput = (event: any) => {
-		if (!event.target.value) return handleFetchResults([]);
+		if (!event.target.value) return handleFetchResults(null);
 
 		fetch(
 			`https://api.mapbox.com/geocoding/v5/mapbox.places/${event.target.value}}.json?access_token=${process.env.REACT_APP_MAPBOX_KEY}`
@@ -16,8 +41,29 @@ const GeoSearch: FC<IGeoSearchTypes> = ({ handleFetchResults }) => {
 			})
 			.catch((err) => {
 				console.error("An error occured while trying to find location:", err);
-				handleFetchResults([]);
+				handleFetchResults(null);
 			});
+	};
+
+	/**
+	 * Here the results of the @function handleSearchInput will be handled.
+	 * The useable data gets filtered and are pushed to the store.
+	 *
+	 * @param results This is an array of the 5 best matches returned from
+	 * the Mapbog GL API.
+	 */
+	const handleFetchResults = (results: any) => {
+		if (!results) return dispatch(setLocations([]));
+		const filteredResults: ILocation[] = results.features.map(
+			(element: any): ILocation => {
+				return {
+					name: element.text as string,
+					detailedName: element.place_name as string,
+					center: element.center as Coordinates,
+				};
+			}
+		);
+		dispatch(setLocations(filteredResults));
 	};
 
 	return (
@@ -27,6 +73,7 @@ const GeoSearch: FC<IGeoSearchTypes> = ({ handleFetchResults }) => {
 				className="searchInput"
 				placeholder="Find your destination"
 				onChange={handleSearchInput}
+				ref={inputRef}
 			/>
 		</div>
 	);
