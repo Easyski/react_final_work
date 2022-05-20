5a66b6855764813f59d71efe27e8e60d8f14d2e9
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-20 10:12:12 +0200

    feat(store): update store with marker logic

--- src/store/slices/editorSlice.ts
+++ src/store/slices/editorSlice.ts
@@ -1,11 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 interface IInitialState {
-	mode: "null" | "points" | "tracks" | "routes";
+	mode: null | "points" | "tracks" | "routes";
+	allowNewMarker: boolean;
+	markerModal: boolean;
 }
 
 const initialState: IInitialState = {
-	mode: "null",
+	mode: null,
+	allowNewMarker: false,
+	markerModal: false,
 };
 
 export const editorSlice = createSlice({
@@ -15,9 +19,16 @@ export const editorSlice = createSlice({
 		setMode: (state, action) => {
 			state.mode = action.payload;
 		},
+		setAllowNewMarker: (state, action) => {
+			state.allowNewMarker = action.payload;
+		},
+		setMarkerModal: (state, action) => {
+			state.markerModal = action.payload;
+		},
 	},
 });
 
-export const { setMode } = editorSlice.actions;
+export const { setMode, setAllowNewMarker, setMarkerModal } =
+	editorSlice.actions;
 
 export default editorSlice.reducer;
