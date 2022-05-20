52e0d0ad59da30de6073e8e4f6b1a1d3f6d81616
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-20 10:05:38 +0200

    feat(editor): split into child components to move logic

--- src/components/EditorConfig/EditorConfig.tsx
+++ src/components/EditorConfig/EditorConfig.tsx
@@ -1,41 +1,32 @@
-import { FC } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { FC, useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import { setMode } from "../../store/slices/editorSlice";
+import ModeConfig from "./ModeConfig/ModeConfig";
+
+import { BiLocationPlus } from "react-icons/bi";
+import { FaRoute } from "react-icons/fa";
+import { RiGuideLine } from "react-icons/ri";
 
 import "./EditorConfig.css";
 
 const EditorConfig: FC = () => {
-	const dispatch = useDispatch();
 	const editorMode = useSelector((state: any) => state.editor.mode);
 
-	const handleSelectorClick = (mode: "points" | "tracks" | "routes") => {
-		if (mode !== editorMode) return dispatch(setMode(mode));
-		dispatch(setMode("null"));
-	};
+	useEffect(() => {
+		console.log("EditorConfig: editorMode", editorMode);
+	}, [editorMode]);
 
 	return (
 		<div className="editorContainer">
-			<div
-				className="editorSelector"
-				onClick={() => handleSelectorClick("points")}
-			>
-				1
-			</div>
-			<div
-				className="editorSelector"
-				onClick={() => handleSelectorClick("routes")}
-				defaultValue="tracks"
-			>
-				2
-			</div>
-			<div
-				className="editorSelector"
-				onClick={() => handleSelectorClick("tracks")}
-				defaultValue="routes"
-			>
-				3
-			</div>
+			<ModeConfig mode="points" selectedMode={editorMode}>
+				<BiLocationPlus />
+			</ModeConfig>
+			<ModeConfig mode="tracks" selectedMode={editorMode}>
+				<RiGuideLine />
+			</ModeConfig>
+			<ModeConfig mode="routes" selectedMode={editorMode}>
+				<FaRoute />
+			</ModeConfig>
 		</div>
 	);
 };
