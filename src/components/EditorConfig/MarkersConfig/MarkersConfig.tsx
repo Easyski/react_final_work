f63abd8a7f1df037164769bf73f6b84edbaceac3
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-21 10:04:57 +0200

    feat(marker): add marker to panel upon map click

---
+++ src/components/EditorConfig/MarkersConfig/MarkersConfig.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { useSelector } from "react-redux";
+import MarkerOption from "../MarkerOption/MarkerOption";
+
+import "./MarkersConfig.css";
+
+const MarkersConfig: FC = () => {
+	const markersToBeAdded = useSelector((store: any) => store.editor.newMarkers);
+
+	const handleMarkersAdded = () => {
+		if (!markersToBeAdded[0]) return;
+		const markersAsElements = markersToBeAdded.map(
+			(marker: any, index: number) => {
+				return <MarkerOption center={marker} key={index} />;
+			}
+		);
+
+		return markersAsElements;
+	};
+
+	return <div className="markersContainer">{handleMarkersAdded()}</div>;
+};
+
+export default MarkersConfig;
