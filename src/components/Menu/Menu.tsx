f1a9c9d114e5cf7586b5a9f1e0c4069f3766a4b9
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-22 16:20:01 +0200

    feat(menu): add menu component

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { BiMenu } from "react-icons/bi";
+
+const Menu: FC = () => {
+	return (
+		<div className="menu flex-h">
+			<h2>menu</h2>
+			<BiMenu />
+		</div>
+	);
+};
+
+export default Menu;
