57d03a468d31d4f6abf8e38370e71613bf19a2c9
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-22 10:34:25 +0200

    feat(map): add location name to store

--- src/store/slices/mapSlice.ts
+++ src/store/slices/mapSlice.ts
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
 	centerCoordinates: [4.34878, 50.85045],
 	zoom: 6,
+	locationName: "",
 };
 
 export const mapSlice = createSlice({
@@ -15,9 +16,13 @@ export const mapSlice = createSlice({
 		setZoom: (state, action) => {
 			state.zoom = action.payload;
 		},
+		setLocationName: (state, action) => {
+			state.locationName = action.payload;
+		},
 	},
 });
 
-export const { setCenterCoordinates, setZoom } = mapSlice.actions;
+export const { setCenterCoordinates, setZoom, setLocationName } =
+	mapSlice.actions;
 
 export default mapSlice.reducer;
