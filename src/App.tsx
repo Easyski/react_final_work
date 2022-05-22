86f79fa9502818ccd1228509134189c8c38a4961
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-22 16:20:23 +0200

    feat(menu): add menu to app

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Map from "./pages/map.page";
 import store from "./store";
 
 import "./styles/main.scss";
+import { Menu } from "./components";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY as string;
 
@@ -15,6 +16,7 @@ export const App: FC = () => {
 	return (
 		<Provider store={store}>
 			<Router>
+				<Menu />
 				<Routes>
 					<Route path="/" element={<p>HOME</p>} />
 					<Route path="/map" element={<Map />} />
