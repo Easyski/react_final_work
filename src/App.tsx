3bbfe3fbce0713791a45d484ea1c23f40b7162be
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-22 15:06:46 +0200

    feat(router): add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 // import mapboxgl from "mapbox-gl";
-import mapboxgl from "mapbox-gl";
 import { FC } from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import mapboxgl from "mapbox-gl";
+
 import Map from "./pages/map.page";
 import store from "./store";
+
 import "./styles/main.scss";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY as string;
@@ -11,10 +14,12 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY as string;
 export const App: FC = () => {
 	return (
 		<Provider store={store}>
-			{/* <Navigation />
-			<Topbar />
-			<Sidebar /> */}
-			<Map />
+			<Router>
+				<Routes>
+					<Route path="/" element={<p>HOME</p>} />
+					<Route path="/map" element={<Map />} />
+				</Routes>
+			</Router>
 		</Provider>
 	);
 };
