b54adbfc8b15805c16c8e5d189f05dae49b18446
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-23 22:47:50 +0200

    feat(sidebar): change editor to sidebar

--- src/store/slices/sidebarSlice.ts
+++ src/store/slices/sidebarSlice.ts
@@ -1,1 +1,19 @@
-export {};
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	newMarkers: [],
+};
+
+export const sidebarSlice = createSlice({
+	name: "sidebarSlice",
+	initialState,
+	reducers: {
+		setNewMarkers: (state, action) => {
+			state.newMarkers = action.payload;
+		},
+	},
+});
+
+export const { setNewMarkers } = sidebarSlice.actions;
+
+export default sidebarSlice.reducer;
