c2aafd87a15558c4d8dceea8d4fcefb951b6637e
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-29 09:13:41 +0200

    feat(firebase): sign in with firebase

--- src/hooks/useCredentials.ts
+++ src/hooks/useCredentials.ts
@@ -2,18 +2,32 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const auth = getAuth();
 
-const signIn = (email: string, password: string) => {
-	signInWithEmailAndPassword(auth, email, password)
+interface IUser {
+	user: { uid: string; email: string } | null;
+	signedIn: boolean;
+}
+
+const signIn = (email: string, password: string): Promise<IUser> => {
+	return signInWithEmailAndPassword(auth, email, password)
 		.then((userCredential) => {
 			// Signed in
-			const user = userCredential.user;
-			console.log("USER", user);
-			// ...
+			const user = {
+				uid: userCredential.user.uid,
+				email: userCredential.user.email as string,
+			};
+			console.log(user);
+
+			return { user, signedIn: true };
 		})
 		.catch((error) => {
 			const errorCode = error.code;
 			const errorMessage = error.message;
-			console.error(errorCode, errorMessage);
+
+			console.error(
+				"An error occured while trying to log in. Please try again.",
+				{ code: errorCode, message: errorMessage }
+			);
+			return { user: null, signedIn: false };
 		});
 };
 
