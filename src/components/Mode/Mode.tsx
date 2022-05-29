a0041662bf34d60cd83ba438a67a53c2a89da0e7
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-29 10:38:13 +0200

    feat(cleanup): remove log

--- src/components/Mode/Mode.tsx
+++ src/components/Mode/Mode.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { useSelector } from "react-redux";
 
 import { BiLocationPlus, BiTrip } from "react-icons/bi";
@@ -8,10 +8,6 @@ import ModeItem from "../ModeItem/ModeItem";
 
 const Mode: FC = () => {
 	const selectedMode = useSelector((state: any) => state.topbar.mode);
-
-	useEffect(() => {
-		console.log("Mode: selectedMode", selectedMode);
-	}, [selectedMode]);
 
 	return (
 		<div className="mode flex flex-h">
