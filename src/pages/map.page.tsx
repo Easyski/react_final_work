6e0de0e6044a1c06ad94eef1b4f580a507c0e87d
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-29 10:15:23 +0200

    feat(auth): get points when signed in

--- src/pages/map.page.tsx
+++ src/pages/map.page.tsx
@@ -1,8 +1,15 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
 import { Navigation, Topbar, Sidebar } from "../components";
+import { usePointsCollection } from "../hooks";
 
 const Map: FC = () => {
+	const points = usePointsCollection();
+
+	useEffect(() => {
+		points.then((points) => console.log(points));
+	});
+
 	return (
 		<>
 			<Navigation />
