ee16696bb69b22dd8e369fa63edca7ee9be2160d
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-31 11:45:18 +0200

    feat(link): add link types and update render output

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,14 +1,18 @@
 import { FC } from "react";
 import cn from "classnames";
 
-import { BiChevronRight } from "react-icons/bi";
+import { BiChevronLeft, BiChevronRight, BiHelpCircle } from "react-icons/bi";
 import { ILink } from "./Link.styles";
 
-const Link: FC<ILink> = ({ content, type, extraStyle, onClick }) => {
+const Link: FC<ILink> = ({ content, type, extraStyle, textStyle, onClick }) => {
 	const icon = () => {
 		switch (type) {
 			case "arrow":
-				return <BiChevronRight className="link-icon" />;
+				return <BiChevronRight className={cn("link-icon", textStyle)} />;
+			case "help":
+				return <BiHelpCircle className={cn("link-icon ", textStyle)} />;
+			case "back":
+				return <BiChevronLeft className={cn("link-icon", textStyle)} />;
 		}
 	};
 
@@ -18,12 +22,34 @@ const Link: FC<ILink> = ({ content, type, extraStyle, onClick }) => {
 	};
 
 	return (
-		<div className={cn("flex flex-h align-center pointer", { extraStyle })}>
-			<button className="btn-as-text pointer" onClick={handleOnClick}>
-				{content}
-			</button>
-			{icon()}
-		</div>
+		<>
+			{type === "submit" ? (
+				<button
+					className="submit color-white bold uppercase"
+					type="submit"
+					onClick={handleOnClick}
+				>
+					{content}
+				</button>
+			) : (
+				<div
+					className={cn("flex flex-h align-center pointer contain", extraStyle)}
+					onClick={handleOnClick}
+				>
+					{type === "back" ? (
+						<>
+							{icon()}
+							<p className={cn(textStyle)}>{content}</p>
+						</>
+					) : (
+						<>
+							<p className={cn(textStyle)}>{content}</p>
+							{icon()}
+						</>
+					)}
+				</div>
+			)}
+		</>
 	);
 };
 
