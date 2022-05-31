1b22db5a3953726ed4b07946e048254dfc2f15bc
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-05-31 17:42:32 +0200

    feat(hook): use navigate hook

--- src/components/AuthRoute/AuthRoute.tsx
+++ src/components/AuthRoute/AuthRoute.tsx
@@ -1,14 +1,17 @@
 import { FC, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const AuthRoute: FC<{ route: JSX.Element }> = ({ route }) => {
-	// const [user, setUser] = useState<boolean>(false);
+	const navigate = useNavigate();
 	const loggedIn = useSelector((store: any) => store.user.loggedIn);
 
-	useEffect(() => {}, [loggedIn]);
+	useEffect(() => {
+		if (!loggedIn) navigate("/", { replace: true });
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [loggedIn]);
 
-	if (!loggedIn) return <Navigate to={"/"} />;
+	if (!loggedIn) return null;
 	return route;
 };
 
