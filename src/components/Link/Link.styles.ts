31cf5971d285d7155e26e949df1fc0f6cd954d49
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-06-01 23:07:57 +0200

    feat(link): add delete type

--- src/components/Link/Link.styles.ts
+++ src/components/Link/Link.styles.ts
@@ -1,6 +1,6 @@
 export interface ILink {
 	content: string;
-	type: "arrow" | "help" | "back" | "submit" | "google";
+	type: "arrow" | "help" | "back" | "submit" | "google" | "delete";
 	onClick: () => void;
 	extraStyle?: string;
 	textStyle?: string;
