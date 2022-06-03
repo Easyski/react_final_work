b09a4b3b794cf6130aee774c522d71ebbdeafaa2
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-06-03 17:23:28 +0200

    feat(style): change style

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -7,7 +7,7 @@ import { GoSearch } from "react-icons/go";
 const Topbar: FC<ITopbar> = () => {
 	return (
 		<div className="topbar container flex">
-			<GoSearch className="searchIcon font-xl self-align-center" />
+			<GoSearch className="searchIcon font-xl align-self-center" />
 			<GeoSearch />
 			<Mode />
 			<SearchResults />
