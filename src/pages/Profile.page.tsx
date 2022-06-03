c9d0783a7551137dc93cb591ac61e78050e777fb
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-06-03 17:24:40 +0200

    feat(profile): add settings and account deletion

--- src/pages/Profile.page.tsx
+++ src/pages/Profile.page.tsx
@@ -1,21 +1,96 @@
-import { FC } from "react";
-import { Navigate } from "react-router-dom";
-import { Input, Link } from "../components";
-import { getUser } from "../hooks/useCredentials";
+import { FC, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import cn from "classnames";
+
+import { Input, Link, Loading, Setting } from "../components";
+import fallbackImage from "../assets/fallback.png";
+import { deleteSignedUser } from "../hooks/useCredentials";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Profile: FC = () => {
-	// Get userdata here
+	const navigate = useNavigate();
+	const user = useSelector((store: any) => store.user.userData);
 
-	const { user } = getUser();
-	console.log("Profile", user);
+	const formRef = useRef<HTMLFormElement>(null);
+	const [deleteSafety, setDeleteSafety] = useState<boolean>(false);
+	const [deleting, setDeleting] = useState<boolean>(false);
+	const profileImage = user.image || fallbackImage;
 
-	if (!user) return <Navigate to="/" />;
+	const settings = [
+		{
+			title: "Profile visibility",
+			content:
+				"Allow other people to find your profile by searching for it in the app.",
+			enabled: true,
+		},
+		{
+			title: "Display profile picture",
+			content: "Allow other people to view your profile picture.",
+			enabled: false,
+		},
+		{
+			title: "Default route publish",
+			content:
+				"Toggle wether other people can find, use and share your routes by default. This can be set for each route individually. This will only affect new routes.",
+			enabled: true,
+		},
+		{
+			title: "Profile visibility",
+			content:
+				"Allow other people to find your profile by searching for it in the app.",
+			enabled: true,
+		},
+		{
+			title: "Data analysation",
+			content: "Personalised routes recommended to you by our AI.",
+			enabled: false,
+		},
+	];
+
+	const renderSettings = (): JSX.Element[] => {
+		return settings.map((el: any) => {
+			return (
+				<Setting
+					title={el.title}
+					content={el.content}
+					defaultValue={el.enabled}
+				/>
+			);
+		});
+	};
+
+	const handleDeleteAccount = () => {
+		if (!deleteSafety) {
+			setDeleteSafety(true);
+			setTimeout(() => setDeleteSafety(false), 15000);
+			return;
+		}
+		formRef.current &&
+			formRef.current.dispatchEvent(
+				new Event("submit", { cancelable: true, bubbles: true })
+			);
+	};
+
+	const handleDeleteForm = async (e: any) => {
+		e.preventDefault();
+		setDeleting(true);
+		const { error } = await deleteSignedUser(e.target[0].value);
+		if (error) {
+			toast.error("The account has not been deleted.");
+			toast.error("The password is incorrect.");
+			setDeleting(false);
+			return;
+		}
+		setDeleting(false);
+		// navigate("/", { replace: true });
+	};
 
 	return (
 		<div className="profile full-view-width-heigth grid grid-two-col grid-center">
 			<div className="left">
 				<div className="container image circle">
-					<img src={user.image} alt="" className="full-width-height" />
+					<img src={profileImage} alt="" className="full-width-height" />
 				</div>
 				<div className="data margin-auto grid">
 					<Input label="Name" value={user.name} readOnly type="text" />
@@ -29,14 +104,41 @@ const Profile: FC = () => {
 					type="google"
 					extraStyle="google profile-button margin-auto"
 				/>
-				<Link
-					content="Delete your account"
-					onClick={() => console.log("GUUGLE")}
-					type="delete"
-					extraStyle="margin-auto margin-top-lg"
-				/>
+				{deleteSafety && !deleting && (
+					<form
+						onSubmit={handleDeleteForm}
+						className="deleteForm margin-top-lg margin-auto"
+						ref={formRef}
+					>
+						<Input
+							label="Please enter your password"
+							type="password"
+							onSubmit={handleDeleteForm}
+							extraStyle="deleteInput"
+						/>
+					</form>
+				)}
+				{!deleting ? (
+					<Link
+						content={
+							deleteSafety
+								? "Are you sure? This is irreversible!"
+								: "Delete your account"
+						}
+						onClick={handleDeleteAccount}
+						type="delete"
+						extraStyle={cn(
+							"margin-auto",
+							deleteSafety ? "color-red margin-top-sm" : "margin-top-lg"
+						)}
+					/>
+				) : (
+					<div className="loading margin-top-lg">
+						<Loading />
+					</div>
+				)}
 			</div>
-			<div className="right"></div>
+			<div className="right full-width-height">{renderSettings()}</div>
 		</div>
 	);
 };
