6131361511f662ad71ed188ae74d9a0545993750
Wouter Heirstrate <wouter.heirstrate@studiohyperdrive.be>
2022-06-19 10:19:50 +0200

    feat(alt): wrap in try catch

--- src/hooks/getAltitude.ts
+++ src/hooks/getAltitude.ts
@@ -1,8 +1,11 @@
 const getAltitude = async (lat: number, lng: number): Promise<number> => {
-	const req = await fetch(`http://localhost:3001/alt?lat=${lat}&lng=${lng}`);
-	const { alt } = await req.json();
-	if (alt) return alt as number;
-	return -1;
+	try {
+		const req = await fetch(`http://localhost:3001/alt?lat=${lat}&lng=${lng}`);
+		const { alt } = await req.json();
+		return alt as number;
+	} catch (error: any) {
+		return -1;
+	}
 };
 
 export { getAltitude };
